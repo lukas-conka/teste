50c53e7eb9e53788e9f04f3a283b5921a0f3f11c
Lucas Amaral <lukas@hotmail.co.jp>
2018-11-27 19:52:25 -0200

    primeiro commit

--- app/src/main/java/com/example/mt4_pc/teste/BemVindoActivity.java
+++ app/src/main/java/com/example/mt4_pc/teste/BemVindoActivity.java
@@ -1,10 +1,12 @@
 package com.example.mt4_pc.teste;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.app.Activity;
@@ -16,12 +18,28 @@ public class BemVindoActivity extends DebugActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bem_vindo);
 
+        /*Intent intent = getIntent();
+        Bundle args = intent.getExtras();
+        String nome = args.getString("nome");*/
+
         Bundle args = getIntent().getExtras();
         String nome = args.getString("nome");
         TextView text = (TextView) findViewById(R.id.text);
         text.setText(nome + ", seja bem-vindo");
-
-
+        //add o bottao "up navigation"
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+  /* @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            //o metodo finish() vai encerrar essa activity
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+*/
 
 }
--- app/src/main/java/com/example/mt4_pc/teste/MainActivity.java
+++ app/src/main/java/com/example/mt4_pc/teste/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.mt4_pc.teste;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends DebugActivity {
+
+    //final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,30 @@ public class MainActivity extends AppCompatActivity {
             return new Button.OnClickListener(){
                 public void onClick(View view){
 
+                    TextView tLogin = (TextView) findViewById(R.id.tLogin);
+                    TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                    String login = tLogin.getText().toString();
+                    String senha = tSenha.getText().toString();
+                    if("lucas".equals(login) && "123".equals(senha)){
+                        Intent intent = new Intent(getContext(), BemVindoActivity.class);
+                        Bundle params = new Bundle();
+                        params.putString("nome", "Lucas Amaral");
+                        //intent.putExtra("nome", "Lucas amaral"); usado tbm para passar parametros para Intent
+                        intent.putExtras(params);
+                        startActivity(intent);
+                    }else{
+                        alert("Login e senha incorretos");
+                    }
+
                 }
             };
+        }
+
+        private Context getContext(){
+            return this;
+        }
+
+        private void alert(String s){
+            Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
         }
 }
