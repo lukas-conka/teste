a12fb9823aae7f1b43bf5c53b0c98a3950526348
Lukas <lukas@hotmail.co.jp>
2018-11-28 23:40:27 -0200

    insercao do menu

--- app/src/main/java/com/example/mt4_pc/teste/MainActivity.java
+++ app/src/main/java/com/example/mt4_pc/teste/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +21,13 @@ public class MainActivity extends DebugActivity {
         setContentView(R.layout.activity_main);
         Button btLogin = (Button) findViewById(R.id.btLogin);
         btLogin.setOnClickListener(onClickLogin());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Infla o menu com os botoes da action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
         //metodo onCLickLogin() retorna uma implmentacao de View.onClickListener
